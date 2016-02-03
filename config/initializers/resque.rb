73ff9a793aa3e23995988ca348510410e482132e
David Conner <dconner.pro@gmail.com>
2016-02-03 01:28:48 -0500

    update env var names for redis/resque
    * prefixing most non-standard, application environment vars with "STG_"
    
    * this is because it makes it easier to containerize the application later

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,8 +3,8 @@ require 'mono_logger'
 # TODO: remove references to Rails.root and Rails.env,
 # - so this file can boot resque outside of rails (when resque-web is booted within sinatra)
 
-$redis = if ENV["REDISCLOUD_URL"]
-           Redis.new(:url => ENV["REDISCLOUD_URL"])
+$redis = if ENV["STG_REDISCLOUD_URL"]
+           Redis.new(:url => ENV["STG_REDISCLOUD_URL"])
          else
            resque_config = YAML.load(File.join(Rails.root, '/config/redis.yml'))
            resque_config[Rails.env]
