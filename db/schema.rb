9f2061e4c66cf4215ca41f34a85cdfef76aa8efd
David Conner <dconner.pro@gmail.com>
2016-02-03 01:42:11 -0500

    add schema.rb, after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160131064617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "rooms_positions"
+    t.string   "color"
+    t.string   "background_image_url"
+    t.integer  "height",               default: 100
+    t.integer  "width",                default: 100
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.integer  "user_id"
+  end
+
+  create_table "local_participation_records", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "board_id"
+    t.integer  "num_moves"
+    t.integer  "num_invites"
+    t.integer  "xpos"
+    t.integer  "ypos"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.integer  "top_left_x"
+    t.integer  "top_left_y"
+    t.integer  "height"
+    t.integer  "width"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "turns", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "friend_username"
+    t.integer  "board_id"
+    t.string   "command"
+    t.integer  "xpos"
+    t.integer  "ypos"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "message"
+    t.integer  "local_participation_record_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.text     "profile_photo_url"
+    t.string   "twitter_user_id"
+  end
 
 end
