1d573d99e1b613730ccd91a928a6d4283fe3bcd6
David Conner <dconner.pro@gmail.com>
2016-02-03 01:42:19 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
+require 'resque_web'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :turns
+  resources :regions
+  resources :local_participation_records
+  resources :boards
+  resources :user
+
+  root 'boards#show'
+
+  mount ResqueWeb::Engine => '/resque_web'
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
