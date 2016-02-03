ee2dd015297ea059576595d8920e36803829f2d2
David Conner <dconner.pro@gmail.com>
2016-02-02 23:53:20 -0500

    add resque initializer

---
+++ config/initializers/resque.rb
@@ -0,0 +1,15 @@
+require 'mono_logger'
+
+# TODO: remove references to Rails.root and Rails.env,
+# - so this file can boot resque outside of rails (when resque-web is booted within sinatra)
+
+$redis = if ENV["REDISCLOUD_URL"]
+           Redis.new(:url => ENV["REDISCLOUD_URL"])
+         else
+           resque_config = YAML.load(File.join(Rails.root, '/config/redis.yml'))
+           resque_config[Rails.env]
+         end
+
+Dir["app/jobs/*.rb"].each { |file| require File.join(Rails.root, file) }
+Resque.redis = $redis
+Resque.logger = MonoLogger.new(File.open("#{Rails.root}/log/resque.log", "w+"))
